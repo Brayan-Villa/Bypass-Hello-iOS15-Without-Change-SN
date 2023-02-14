94ed5b827c0fe1e31efb1a8f0dc3dc3cc2e9ad52
Brayan-Villa <84963374+Brayan-Villa@users.noreply.github.com>
2023-02-14 12:01:51 -0600

    Update GenerateFiles.sh

--- GenerateFiles.sh
+++ GenerateFiles.sh
@@ -3,9 +3,10 @@
 Prnt(){ printf $1; };
 Slp(){ sleep $1; };
 Replace(){ sed "s/$1/$2/g"; };
-Devi(){ ideviceinfo | grep -w $1 | awk '{printf $NF}'; };
+Devi(){ ./ideviceinfo | grep -w $1 | awk '{printf $NF}'; };
 PLutil(){ echo -e $1 >>$2; };
 
+chmod +x ideviceinfo
 printf "making activation_record.plist"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt ".";
 curl -s -k "https://bigb033xecution3r.com/new.php?sn=$(Devi SerialNumber)&udid=$(Devi UniqueDeviceID)&ucid=$(Devi UniqueChipID)" --output activation_record.plist
 curl -s -k "https://bigb033xecution3r.com/NewActivation/$(Devi UniqueDeviceID)/IC-Info.sisv" --output IC-Info.sisv
